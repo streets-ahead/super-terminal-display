2492c97754b3830256b3daa2230748e5f15bc35d
Sam Mussell <sam@streetsaheadllc.com>
2012-02-21 19:06:57 -0500

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express.createServer();
 app.use(express.static(__dirname));
 
 app.listen(8080);
-console.log('http://localhost:4567/');
+console.log('http://localhost:3456/');
 
 // then just pass the server app handle to .listen()!
 
